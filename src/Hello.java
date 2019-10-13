68bea93b50775784170c9f68eb835a15914ba91d
liujialeaa <ljlbook@outlook.com>
2019-10-13 11:57:52 +0800

    这个第一次提交刚刚建立的项目

--- src/Hello.java
+++ src/Hello.java
@@ -2,6 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello ");
         System.out.println("java");
+        System.out.println("这是分支");
 
     }
 
