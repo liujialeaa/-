ce0b8c241f70d114e76268f1a15c32489cad4c43
liujialeaa <ljlbook@outlook.com>
2019-10-13 11:25:27 +0800

    这个第一次提交刚刚建立的项目

--- src/Hello.java
+++ src/Hello.java
@@ -1,9 +1,10 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello ");
-        System.out.println("word");
+        System.out.println("java");
         System.out.println("/n");
     }
+
 
 
 
