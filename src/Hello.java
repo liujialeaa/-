e63f419e4943aaa32bddc79410de17b49bcb16c4
liujialeaa <ljlbook@outlook.com>
2019-10-13 12:32:45 +0800

    这个第一次提交刚刚建立的项目

--- src/Hello.java
+++ src/Hello.java
@@ -1,7 +1,8 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello ");
-        System.out.println("java");
+        System.out.println("jav" +
+                "a");
 
     }
 
